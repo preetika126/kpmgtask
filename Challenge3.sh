9ffc3473e7db8f2cce6f6aafdf491f1079f76793
preetika126 <sahoopreetika@gmail.com>
2024-05-16 13:09:37 +0530

    Update Challenge3.sh

--- Challenge3.sh
+++ Challenge3.sh
@@ -1,30 +1,16 @@
 #!/bin/bash
 
 get_nested_value() {
-    local obj="$1"
-    local key="$2"
-    local IFS='/'
-    local keys=($key)
-    local current_obj="$obj"
-
-    for k in "${keys[@]}"; do
-        current_obj=$(echo "$current_obj" | jq -r --arg k "$k" '.[$k]')
-        if [ "$current_obj" == "null" ]; then
-            echo "null"
-            return
-        fi
-    done
-
-    echo "$current_obj"
+  echo "$1" | jq -r --argjson keys "$(echo '["'$(echo "$2" | sed 's|/|","|g')'"]')" 'getpath($keys)'
 }
 
-# Example usage:
-obj1='{"a":{"b":{"c":"d"}}}'
+# Example usage
+json1='{"a":{"b":{"c":"d"}}}'
 key1="a/b/c"
-result1=$(get_nested_value "$obj1" "$key1")
-echo "Result 1: $result1"  # Output: d
+value1=$(get_nested_value "$json1" "$key1")
+echo "Result 1: $value1"  # Output: d
 
-obj2='{"x":{"y":{"z":"a"}}}'
+json2='{"x":{"y":{"z":"a"}}}'
 key2="x/y/z"
-result2=$(get_nested_value "$obj2" "$key2")
-echo "Result 2: $result2"  # Output: a
+value2=$(get_nested_value "$json2" "$key2")
+echo "Result 2: $value2"  # Output: a
